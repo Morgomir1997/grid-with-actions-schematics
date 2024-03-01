5f29de37fa65a9dc8647acb9fd83110883dff65f
Marcell Veress <marcell.veress@gmail.com>
2024-03-01 16:01:28 +0100

    Project setup

--- src/grid-with-actions-schematics/index.ts
+++ src/grid-with-actions-schematics/index.ts
@@ -4,6 +4,9 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function gridWithActionsSchematics(_options: any): Rule {
+
+  console.log('Hello from your new schematic!')
+
   return (tree: Tree, _context: SchematicContext) => {
     return tree;
   };
