2e0d18cfd0427a0cfe99d35cc5f5865bcc6d515b
Marcell Veress <marcell.veress@gmail.com>
2024-03-03 15:31:46 +0100

    Outputdir change and file reading added

--- src/grid-with-actions-schematics/index.ts
+++ src/grid-with-actions-schematics/index.ts
@@ -1,15 +1,31 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+// import text from './source-files/grid-main.component';
+import * as fs from 'fs';
+import * as path from 'path';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function gridWithActionsSchematics(_options: any): Rule {
 
+  // const dirContents = fs.readdirSync('./src/grid-with-actions-schematics/source-files/grid-main.component.ts');
+  // const dirContents = fs.readdirSync('./src/grid-with-actions-schematics/source-files');
+  // console.log(dirContents);
+
+  const fileContents: string = fs.readFileSync(
+    path.join('./src/grid-with-actions-schematics/source-files', 'grid-main.component.ts'),
+    {
+      encoding: 'utf-8',
+    },
+  );
+
+  console.log(fileContents);
+
   console.log('Hello from your new schematic!')
 
   return (tree: Tree, _context: SchematicContext) => {
     // tree.create(_options.name || 'hello', 'world');
-    tree.create(_options.directory, "testFileContent");
+    tree.create(_options.directory, fileContents);
     return tree;
   };
 }
---
+++ src/grid-with-actions-schematics/source-files/grid-main.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+// import { RowType, colDefs, defaultColDef } from './grid-col-def';
+// import { GridActionButtonsComponent } from './grid-action-buttons/grid-action-buttons.component';
+import { BehaviorSubject, Observable } from 'rxjs';
+// import { ActionExecutionComponent } from './action-execution/action-execution.component';
+// import { ColDef, GridApi, GridReadyEvent, Module } from '@ag-grid-community/core';
+import { GridReadyEvent, Module } from '@ag-grid-community/core';
+import { AgGridModule } from '@ag-grid-community/angular';
+import { CommonModule } from '@angular/common';
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+
+@Component({
+  selector: 'grid-main',
+  standalone: true,
+  imports: [AgGridModule, CommonModule ],
+  templateUrl: './grid-main.component.html',
+  styleUrls: ['./grid-main.component.scss']
+})
+export class GridMainComponent {
+  public readonly themeClass = 'ag-theme-quartz';
+  public action = new BehaviorSubject<string|null>(null);
+  public modules: Module[] = [ClientSideRowModelModule];
+  public context: this;
+
+  // private gridApi!: GridApi;
+  private gridApi!: any;
+
+  public rowData = [
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ];
+
+  // public colDefs: ColDef[] = colDefs;
+  // public defaultColDef: ColDef = defaultColDef;
+
+  constructor() {
+    this.context = this;
+  }
+
+  public onGridReady(event: GridReadyEvent) {
+    this.gridApi = event.api;
+    console.log(this.gridApi);
+  }
+
+  public setAction(action: string|null): void {
+    this.action.next(action);
+  }
+
+  public actionDone(): void {
+    this.action.next(null);
+  }
+
+  public get isActionSettled(): Observable<string|null> {
+    return this.action.asObservable();
+  }
+
+  // public get selectedRows(): RowType[]|null {
+  //   return this.gridApi.getSelectedRows();
+  // }
+}
